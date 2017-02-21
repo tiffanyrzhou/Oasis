26005593f1c712fceee69231fbf311c3672349d7
Tiffany R Zhou <tiffanyrzhou@gmail.com>
2017-02-20 20:44:40 -0500

    added javadocs to UserType

--- app/src/main/java/com/turboocelots/oasis/models/UserType.java
+++ app/src/main/java/com/turboocelots/oasis/models/UserType.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 
 /**
  * Created by Tiffany on 2/20/17.
+ * Enum Class of different User Types
  */
 
 public enum UserType implements Serializable {
@@ -12,13 +13,20 @@ public enum UserType implements Serializable {
     Manager("Manager"),
     Administrator ("Administrator");
 
-
     private final String userTypeName;
 
+    /**
+     * Default constructer for enum type
+     * @param userTypeName
+     */
     UserType(String userTypeName){
         this.userTypeName = userTypeName;
     }
 
+    /**
+     *  Returns a String representation of the UserType
+     * @return the UserTypeName
+     */
     private String getUserTypeName(){
         return userTypeName;
     }
