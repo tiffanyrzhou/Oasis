62328e44fd4ab088be56a4d331c8c994bfc97efd
Shane Lyons <Shane@lawn-143-215-82-235.lawn.gatech.edu>
2017-02-20 19:03:07 -0500

    new account types

---
+++ app/src/main/java/com/turboocelots/oasis/models/Administrator.java
@@ -0,0 +1,14 @@
+package com.turboocelots.oasis.models;
+
+import com.turboocelots.oasis.models.Manager;
+
+/**
+ * Created by Shane on 2/20/17.
+ */
+
+public class Administrator extends Manager {
+
+    public Administrator(String username, String password) {
+        super(username, password);
+    }
+}
---
+++ app/src/main/java/com/turboocelots/oasis/models/Worker.java
@@ -0,0 +1,14 @@
+package com.turboocelots.oasis.models;
+
+import com.turboocelots.oasis.models.Reporter;
+
+/**
+ * Created by Shane on 2/20/17.
+ */
+
+public class Worker extends Reporter {
+
+    public Worker(String username, String password) {
+        super(username, password);
+    }
+}
